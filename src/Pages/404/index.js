177a27f53eb276346986a63251180cba99559bf8
Alvaro Paçó <alvaropaconeto@gmail.com>
2020-08-30 19:46:13 -0300

    feat: add routes to the application

---
+++ src/Pages/404/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import notFound from '../../static/images/404_website.svg';
+import '../../styles/Home.scss';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={notFound} className="App-logo" alt="logo" />
+        <p>
+          Paǵina não encontradada.
+        </p>
+        <Link to="/">
+          Voltar á Home
+        </Link>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
